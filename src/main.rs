9f390f39447d462294c41262f09ca3f3f651e3b8
Jonas Ringblom <jonas.ringblom@jenseneducation.se>
2024-04-23 22:18:05 +0200

    Added suggestion to my answer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("hello, world!");
     println!("I'm a Rustacean! (how do you say it?)");
+    println!("I'm a Rustacean! (Rust-ay-shun?)");
     println!("I'm a Rustacean! (Rust-ee--can!)");
 }
